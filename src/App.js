0b4cb4e93821ae6c72fb0c671d5f082ab278d8d7
Vishal Aggarwal <vishalaggarwal541@gmail.com>
2023-06-07 02:53:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './Components/Navbar.js';
+
+function App() {
+  return (
+    <>
+     <NavBar title = "TextUtils"/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/App1.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App1() {
+  return (
+    <>
+    <div classname ="blank1">Hello world </div>
+    <div classname ="blank">Hello world test data</div>
+    <div className="vishal"> hello Vishal Aggarwal</div>
+    <div className="vishal2"><h1> dtggfnfn</h1> hello world</div>
+    <div className="vishal3"> vishal</div>
+    </>
+  );
+}
+
+export default App1;
