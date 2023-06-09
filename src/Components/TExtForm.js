4ef9d20985a942ed6651debd08812e3eb8c0744b
Vishal Aggarwal <vishalaggarwal541@gmail.com>
2023-06-09 22:41:12 +0530

    After adding router

--- src/Components/TExtForm.js
+++ src/Components/TExtForm.js
@@ -17,6 +17,13 @@ export default function TExtForm(props) {
         settext(newtext)
 
     }
+    const clearf = ()=>{
+        console.log("Buttoncliccked")
+        let newtext = '';
+        console.log(newtext)
+        settext(newtext)
+
+    }
     // const [text, settext] = useState('Enter the text');
     const handlechange = (event)=>{
         console.log("changed")
@@ -36,6 +43,7 @@ export default function TExtForm(props) {
         </div>
             <button className="btn btn-primary mx-1 my-1" onClick={handleupclick}>Click here to convert to uppercase</button>
             <button className="btn btn-primary mx-1 my-1" onClick={handledownclick}>Click here to convert to lowercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={clearf}>Click here to clear</button>
         
         
         {/* <div className="mb-3"> 
