db329e26488483e2e3b80cda6c5af0496bfebf21
Vishal Aggarwal <vishalaggarwal541@gmail.com>
2023-06-09 22:36:02 +0530

    After adding router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import NavBar from './Components/Navbar.js';
+import Navbar from './Components/Navbar.js';
+import TExtForm from './Components/TExtForm';
+import About from './Components/About';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-     <NavBar title = "TextUtils"/>
+      <BrowserRouter>
+      <div className="container">
+      <Navbar title="TextUtils"/>
+	<Routes>
+		<Route path="/about" element={<About />}></Route>	
+		<Route path="/" element={<TExtForm />}></Route>	
+   
+	 </Routes>
+
+      </div>
+
+</BrowserRouter>
     </>
   );
 }
 
 export default App;
+
---
+++ src/Components/TExtForm.js
@@ -0,0 +1,75 @@
+import React,{ useState } from 'react'
+// import PropTypes from 'prop-types'
+
+export default function TExtForm(props) {
+    const [text, settext] = useState('');
+    const handleupclick = ()=>{
+        console.log("Buttoncliccked")
+        let newtext = text.toUpperCase();
+        console.log(newtext)
+        settext(newtext)
+
+    }
+    const handledownclick = ()=>{
+        console.log("Buttoncliccked")
+        let newtext = text.toLowerCase();
+        console.log(newtext)
+        settext(newtext)
+
+    }
+    // const [text, settext] = useState('Enter the text');
+    const handlechange = (event)=>{
+        console.log("changed")
+        settext(event.target.value)
+
+    }
+    return (
+        <div>
+
+
+        <div classname="mb-3"> 
+            {
+                <h1> Enter Text to convert</h1>
+            /* <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label> */}
+            <textarea className="form-control" value={text} placeholder='Enter the text' onChange={handlechange} id="myBox" rows="10"></textarea>
+            {/* <textarea className="form-control"  id="myBox" rows="8"></textarea> */}
+        </div>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleupclick}>Click here to convert to uppercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handledownclick}>Click here to convert to lowercase</button>
+        
+        
+        {/* <div className="mb-3"> 
+            <textarea className="form-control" value={text}  id="myBox" rows="8"></textarea>
+            </div> */}
+        </div>
+
+    )
+}
+// import React, {useState} from 'react'
+
+
+// export default function TextForm() {
+//     // const handleUpClick = ()=>{
+//     //     // console.log("Uppercase was clicked: " +  text);
+//     //     let newText = text.toUpperCase();
+//     //     setText(newText)
+//     // }
+
+//     // const handleOnChange = (event)=>{
+//     //     // console.log("On change");
+//     //     setText(event.target.value)
+//     // }
+
+//     const [text, setText] = useState('Enter text here2'); 
+//     // // text = "new text"; // Wrong way to change the state
+//     // // setText("new text"); // Correct way to change the state
+//     return (
+//         <div> 
+//             {/* <h1>{props.heading}</h1> */}
+//             <div className="mb-3"> 
+//             <textarea className="form-control" value={text}  id="myBox" rows="8"></textarea>
+//             </div>
+//             {/* <button className="btn btn-primary">Convert to Uppercase</button> */}
+//         </div>
+//     )
+// }
